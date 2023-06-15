f42cfce48fa3ed5735a7adaec1f0de45e5bc239a
sonukolAi <136089232+sonukolAi@users.noreply.github.com>
2023-06-15 14:34:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ publishMessage('mytopic', 'Hello, HiveMQ!');
 
 // Schedule message publication every 5 seconds
 setInterval(() => {
-  publishMessage('mytopic', 'Another message');
+  publishMessage('mytopic', 'hi i am server');
 }, 10000);
 
 // Unsubscribe and disconnect gracefully when you're done
