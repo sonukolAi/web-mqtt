21dfeee7846c1aa46417a4a69bfb0cd0000c3091
sonukolAi <136089232+sonukolAi@users.noreply.github.com>
2023-06-24 14:48:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,47 +6,62 @@ const brokerUrl = 'mqtt://broker.hivemq.com';
 // Create a client instance
 const client = mqtt.connect(brokerUrl);
 
-function setupOnlineHost() {
-  // Set up event handlers for connect, message, and error events
-  client.on('connect', () => {
-    console.log('Connected to MQTT broker');
+// Set up event handlers for connect, message, and error events
+client.on('connect', () => {
+  console.log('Connected to MQTT broker');
 
-    // Subscribe to a topic when connected
-    client.subscribe('mytopic');
+  // Subscribe to a topic when connected
+  client.subscribe('mytopic', (error) => {
+    if (error) {
+      console.error('Error while subscribing:', error);
+    } else {
+      console.log('Subscribed to mytopic');
+    }
   });
+});
 
-  client.on('message', (topic, message) => {
-    console.log(`Received message on topic ${topic}: ${message.toString()}`);
+client.on('message', (topic, message) => {
+  console.log(`Received message on topic ${topic}: ${message.toString()}`);
+});
+
+client.on('error', (error) => {
+  console.error('Error:', error);
+});
+
+// Publish a message to a topic
+function publishMessage(topic, message) {
+  client.publish(topic, message, (error) => {
+    if (error) {
+      console.error('Error while publishing message:', error);
+    } else {
+      console.log(`Published message on topic ${topic}: ${message}`);
+    }
   });
-
-  client.on('error', (error) => {
-    console.error('Error:', error);
-  });
-
-  // Publish a message to a topic
-  function publishMessage(topic, message) {
-    client.publish(topic, message);
-    console.log(`Published message on topic ${topic}: ${message}`);
-  }
-
-  // Example usage: publishing a message and subscribing to a topic
-  publishMessage('mytopic', '!Hello, HiveMQ!');
-
-  // Schedule message publication every 5 seconds
-  setInterval(() => {
-    publishMessage('mytopic', '!hi i am server');
-  }, 5000);
-
-  // Unsubscribe and disconnect gracefully when you're done
-  function cleanUp() {
-    client.unsubscribe('mytopic');
-    client.end();
-    console.log('Unsubscribed and disconnected');
-  }
-
-  // Uncomment the line below if you want to automatically disconnect after 30 seconds
-  // setTimeout(cleanUp, 30000);
 }
 
-// Call the setupOnlineHost function to start the online host
-setupOnlineHost();
+// Example usage: publishing a message and subscribing to a topic
+publishMessage('mytopic', '!Hello, HiveMQ!');
+
+// Schedule message publication every 10 seconds
+setInterval(() => {
+  publishMessage('mytopic', '!hi i am server');
+}, 10000);
+
+// Unsubscribe and disconnect gracefully when you're done
+function cleanUp() {
+  client.unsubscribe('mytopic', (error) => {
+    if (error) {
+      console.error('Error while unsubscribing:', error);
+    } else {
+      console.log('Unsubscribed from mytopic');
+    }
+  });
+  client.end();
+  console.log('Disconnected');
+}
+
+// Call the cleanUp function when you want to clean up and disconnect
+// cleanUp();
+
+// Export the setupOnlineHost function
+export { setupOnlineHost };
