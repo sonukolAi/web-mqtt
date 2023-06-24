a43c721b97ee849af4d29200912b36a07c133164
sonukolAi <136089232+sonukolAi@users.noreply.github.com>
2023-06-24 15:13:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,71 @@
-import mqtt from 'mqtt';
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const mqtt = require('mqtt');
 
-// MQTT broker URL
+const app = express();
+const port = process.env.PORT || 330;
+
+const sources = {
+  google: {
+    url: (num) => `https://www.google.com/search?q=${num}+train+running+status`,
+    processData: async ($, res) => {
+      try {
+        const train = $('div.k9rLYb').eq(0).text().trim();
+        const status = $('div.dK1Bub .rUtx7d').eq(1).text();
+        const delay = $('div.Rjvkvf.MB86Dc').eq(1).text().trim();
+        const currentTime = new Date().toLocaleTimeString('en-US', {
+          timeZone: 'Asia/Kolkata',
+          hour12: false,
+          hour: 'numeric',
+          minute: 'numeric',
+        });
+        res.json({ train, status, delay, currentTime });
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Error occurred');
+      }
+    },
+  },
+};
+
+app.get('/:num', async (req, res) => {
+  try {
+    const { num } = req.params;
+    const selectedSource = sources.google;
+
+    if (!selectedSource) {
+      return res.status(400).send('Invalid source');
+    }
+
+    const url = selectedSource.url(num);
+    const { data } = await axios.get(url, {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+      },
+    });
+
+    const $ = cheerio.load(data);
+    await selectedSource.processData($, res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error occurred');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
 const brokerUrl = 'mqtt://broker.hivemq.com';
+const topic = 'mytopic';
 
-// Create a client instance
 const client = mqtt.connect(brokerUrl);
 
-// Set up event handlers for connect, message, and error events
 client.on('connect', () => {
   console.log('Connected to MQTT broker');
-
-  // Subscribe to a topic when connected
-  client.subscribe('mytopic', (error) => {
-    if (error) {
-      console.error('Error while subscribing:', error);
-    } else {
-      console.log('Subscribed to mytopic');
-    }
-  });
+  client.subscribe(topic);
 });
 
 client.on('message', (topic, message) => {
@@ -28,39 +76,19 @@ client.on('error', (error) => {
   console.error('Error:', error);
 });
 
-// Publish a message to a topic
 function publishMessage(topic, message) {
-  client.publish(topic, message, (error) => {
-    if (error) {
-      console.error('Error while publishing message:', error);
-    } else {
-      console.log(`Published message on topic ${topic}: ${message}`);
-    }
-  });
+  client.publish(topic, message);
+  console.log(`Published message on topic ${topic}: ${message}`);
 }
 
-// Example usage: publishing a message and subscribing to a topic
-publishMessage('mytopic', '!Hello, HiveMQ!');
+publishMessage(topic, '!Hello, HiveMQ!');
 
-// Schedule message publication every 10 seconds
 setInterval(() => {
-  publishMessage('mytopic', '!hi i am server');
-}, 10000);
+  publishMessage(topic, '!hi i am server');
+}, 5000);
 
 function cleanUp() {
-  client.unsubscribe('mytopic', (error) => {
-    if (error) {
-      console.error('Error while unsubscribing:', error);
-    } else {
-      console.log('Unsubscribed from mytopic');
-    }
-  });
+  client.unsubscribe(topic);
   client.end();
-  console.log('Disconnected');
+  console.log('Unsubscribed and disconnected');
 }
-
-// Call the cleanUp function when you want to clean up and disconnect
-// cleanUp();
-
-// Export the setupOnlineHost function
-export { setupOnlineHost };
