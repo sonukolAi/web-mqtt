4a8a1b21f2983d54645ffac8dc5e60f64a9965bd
sonukolAi <136089232+sonukolAi@users.noreply.github.com>
2023-07-09 17:46:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const { v4: uuidv4 } = require('uuid');
 
 // MQTT settings
 const BROKER = 'broker.hivemq.com';
-const TOPIC_NUM = 'train-num-render';
-const SEND_DATA_TOPIC = 'train-render';
+const TOPIC_NUM = 'train-num-render2';
+const SEND_DATA_TOPIC = 'train-render2';
 
 const CLIENT_ID = 'esp8266_' + uuidv4();
 
-const mqttClient = mqtt.connect(`mqtt://${BROKER}`);
+const mqttClient = mqtt.connect(`mqtt://${BROKER}`, {
+  clientId: CLIENT_ID,
+});
 
-let trainNum = '11706/1'; // Default train number
+let trainNum = '15017/2'; // Default train number
 
 function onMessage(topic, message) {
   if (topic === TOPIC_NUM) {
@@ -20,7 +22,13 @@ function onMessage(topic, message) {
 }
 
 mqttClient.on('connect', () => {
-  mqttClient.subscribe(TOPIC_NUM);
+  mqttClient.subscribe(TOPIC_NUM, (err) => {
+    if (err) {
+      console.log('Subscription error:', err);
+    } else {
+      console.log('Subscribed to', TOPIC_NUM);
+    }
+  });
 });
 
 mqttClient.on('message', onMessage);
@@ -29,8 +37,13 @@ function fetchDataAndPublish() {
   try {
     request.get(`https://nodejs--sonukol.repl.co/${trainNum}`, (error, response, body) => {
       if (!error && response.statusCode === 200) {
-        mqttClient.publish(SEND_DATA_TOPIC, body);
-        console.log(body);
+        mqttClient.publish(SEND_DATA_TOPIC, body.toString(), (err) => {
+          if (err) {
+            console.log('Publishing error:', err);
+          } else {
+            console.log('Data published:', body);
+          }
+        });
       } else {
         console.log('Request failed:', error);
       }
@@ -40,7 +53,26 @@ function fetchDataAndPublish() {
   }
 }
 
+mqttClient.on('error', (error) => {
+  console.log('MQTT error:', error);
+});
+
+mqttClient.on('reconnect', () => {
+  console.log('Reconnecting to MQTT broker...');
+});
+
+mqttClient.on('close', () => {
+  console.log('MQTT connection closed.');
+});
+
+mqttClient.on('offline', () => {
+  console.log('MQTT client is offline.');
+});
+
 setInterval(() => {
-  mqttClient.emit('check');
-  fetchDataAndPublish();
-}, 8000);
+  if (mqttClient.connected) {
+    fetchDataAndPublish();
+  } else {
+    console.log('MQTT client is not connected.');
+  }
+}, 9000);
